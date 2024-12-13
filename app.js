78b400e4b6055e001e9b64bd43de5e721e1f145b
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-12-13 16:26:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+let readmoretext1 = document.getElementById("readmoretext1");
+let textcontant1 = document.getElementById("textcontant1");
+
+readmoretext1.onclick = () => {
+    if (textcontant1.style.display === "none") {
+        textcontant1.style.display = "inline";
+        readmoretext1.textContent = "Read Less";
+    } else {
+        textcontant1.style.display = "none";
+        readmoretext1.textContent = "Read More";
+    }
+};
+
+let readmoretext2 = document.getElementById("readmoretext2");
+let textcontant2 = document.getElementById("textcontant2");
+
+readmoretext2.onclick = () => {
+    if (textcontant2.style.display === "none") {
+        textcontant2.style.display = "inline";
+        readmoretext2.textContent = "Read Less";
+    } else {
+        textcontant2.style.display = "none";
+        readmoretext2.textContent = "Read More";
+    }
+};
+
+let readmoretext3 = document.getElementById("readmoretext3");
+let textcontant3 = document.getElementById("textcontant3");
+
+readmoretext3.onclick = () => {
+    if (textcontant3.style.display === "none") {
+        textcontant3.style.display = "inline";
+        readmoretext3.textContent = "Read Less";
+    } else {
+        textcontant3.style.display = "none";
+        readmoretext3.textContent = "Read More";
+    }
+};
+
+let readmoretext4 = document.getElementById("readmoretext4");
+let textcontant4 = document.getElementById("textcontant4");
+
+readmoretext4.onclick = () => {
+    if (textcontant4.style.display === "none") {
+        textcontant4.style.display = "inline";
+        readmoretext4.textContent = "Read Less";
+    } else {
+        textcontant4.style.display = "none";
+        readmoretext4.textContent = "Read More";
+    }
+};
